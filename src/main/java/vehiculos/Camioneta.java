e5023e5c4963849f2dc0023e91c33e6ec5833e1d
JPosada64 <141966165+mquirozr@users.noreply.github.com>
2023-10-01 23:37:46 -0500

    12 intento

--- src/main/java/vehiculos/Camioneta.java
+++ src/main/java/vehiculos/Camioneta.java
@@ -5,8 +5,8 @@ import vehiculos.Vehiculo;
 public class Camioneta extends Vehiculo {
     private boolean volco;
 
-    public Camioneta(String placa, String nombre, double precio, double peso, Fabricante fabricante, boolean volco) {
-        super(placa, 4, 90, nombre, precio, peso, "4X4", fabricante);
+    public Camioneta(String placa, int puertas, String nombre, double precio, double peso, Fabricante fabricante, boolean volco) {
+        super(placa, puertas, 90, nombre, precio, peso, "4X4", fabricante);
         this.volco = volco;
     }
 
