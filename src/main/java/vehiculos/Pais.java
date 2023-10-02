61e3dd95ff12fd9db0196f76102c6f6d0e99d9be
JPosada64 <141966165+mquirozr@users.noreply.github.com>
2023-10-02 18:04:54 -0500

    17 intento

--- src/main/java/vehiculos/Pais.java
+++ src/main/java/vehiculos/Pais.java
@@ -1,15 +1,19 @@
 package vehiculos;
 
 import java.util.HashMap;
+import java.util.List;
+
 import vehiculos.Vehiculo;
 import java.util.Map;
 
 public class Pais {
 
 	private String nombre;
+	private List<Vehiculo> vehiculos;
 
-    public Pais(String nombre) {
+    public Pais(String nombre, List<test.vehiculos.Vehiculo> vehiculos) {
         this.nombre = nombre;
+        this.vehiculos = vehiculos;
     }
 
     public String getNombre() {
@@ -20,25 +24,5 @@ public class Pais {
         this.nombre = nombre;
     }
     
-    public static String paisMasVendedor() {
-        Map<String, Integer> ventasPorPais = new HashMap<>();
-
-        for (Vehiculo vehiculo : vehiculos) {
-            String paisFabricante = vehiculo.getFabricante().getPais().getNombre();
-            ventasPorPais.put(paisFabricante, ventasPorPais.getOrDefault(paisFabricante, 0) + 1);
-        }
-
-        String paisMasVendedor = "";
-        int maxVentas = 0;
-
-        for (Map.Entry<String, Integer> entry : ventasPorPais.entrySet()) {
-            if (entry.getValue() > maxVentas) {
-                maxVentas = entry.getValue();
-                paisMasVendedor = entry.getKey();
-            }
-        }
-
-        return paisMasVendedor;
-    }
     
 }
