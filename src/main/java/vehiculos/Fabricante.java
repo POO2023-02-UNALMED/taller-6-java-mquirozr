c58579bbe939ba392708a719303f84b2475b9d05
JPosada64 <141966165+mquirozr@users.noreply.github.com>
2023-10-02 23:12:30 -0500

    estafue

--- src/main/java/vehiculos/Fabricante.java
+++ src/main/java/vehiculos/Fabricante.java
@@ -1,5 +1,55 @@
 package vehiculos;
 
+import java.util.HashMap;
+import java.util.Map;
+
+public class Fabricante {
+    private String nombre;
+    private Pais pais;
+    private Map<Fabricante, Integer> ventasPorFabricante = new HashMap<>();
+
+    public Fabricante(String nombre, Pais pais) {
+        this.nombre = nombre;
+        this.pais = pais;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Pais getPais() {
+        return pais;
+    }
+
+    public void setPais(Pais pais) {
+        this.pais = pais;
+    }
+
+    public void registrarVenta() {
+        ventasPorFabricante.put(this, ventasPorFabricante.getOrDefault(this, 0) + 1);
+    }
+
+    public static Fabricante fabricaMayorVentas() {
+        int maxVentas = 0;
+        Fabricante fabricanteMasVendedor = null;
+
+        for (Map.Entry<Fabricante, Integer> entry : ventasPorFabricante.entrySet()) {
+            if (entry.getValue() > maxVentas) {
+                maxVentas = entry.getValue();
+                fabricanteMasVendedor = entry.getKey();
+            }
+        }
+
+        return fabricanteMasVendedor;
+    }
+}
+
+/*package vehiculos;
+
 import vehiculos.Pais;
 
 public class Fabricante {
@@ -29,3 +79,4 @@ public class Fabricante {
     }
     
 }
+*/
